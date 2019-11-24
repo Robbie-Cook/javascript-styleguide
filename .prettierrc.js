8c2a6413e2389f0a8b6a1e6536bcf38cc959db01
Robbie Cook <robbieck7@gmail.com>
2019-11-25 09:13:38 +1300

    Added prettier and updated npm name

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+/**
+ * Configuration file for prettier.
+ * 
+ * See https://prettier.io/docs/en/options.html
+ */
+
+module.exports = {
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: "es5",
+}
